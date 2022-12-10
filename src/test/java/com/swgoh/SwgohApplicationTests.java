b7a72331a46054db84824d5ee4e348ebb6ae19e5
zobo666 <48050583+zobo666@users.noreply.github.com>
2022-12-10 15:56:58 +0200

    initial commit

---
+++ src/test/java/com/swgoh/SwgohApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swgoh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwgohApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
